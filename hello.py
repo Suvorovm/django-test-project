bb1c1d4e66e56c9751a88a2888a33460dd93843f
Suvorovm <royming06@gmail.com>
2019-08-29 22:10:19 +0300

    frontend -backend 3

--- hello.py
+++ hello.py
@@ -2,9 +2,8 @@ def printer(firstArg,SeconArg):
     print("xer")
 
 def serverApp (environ,start_response):
-    dataFromUser = environ['QUERY_STRING']
-    print(dataFromUser.decode('utf-8'))
-    resultList = dataFromUser.decode('utf-8').strip().replace("?","").replace("/","").replace("&"," ").split(" ")
+    dataFromUser = environ['QUERY_STRING'] 
+    resultList = dataFromUser.strip().replace("?","").replace("/","").replace("&"," ").split(" ")
     for i in range(0,len(resultList)):
         resultList[i] =(resultList[i] + '\n').encode("utf-8")
     start_response("200 OK", [("Content-Type", "text/plain")])
