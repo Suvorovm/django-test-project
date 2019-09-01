3f016d0477a2bedb08bc48f95c50fbeb653faba9
suvorovm <royming06@gmail.com>
2019-09-01 10:54:14 +0300

    ffix issue

--- ask/hello.py
+++
@@ -1,7 +0,0 @@
-def serverApp (environ,start_response):
-    dataFromUser = environ['QUERY_STRING'] 
-    resultList = dataFromUser.strip().replace("?","").replace("/","").replace("&"," ").split(" ")
-    for i in range(0,len(resultList)):
-        resultList[i] =(resultList[i] + '\n').encode("utf-8")
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    return resultList
