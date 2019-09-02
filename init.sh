ea29c2964b9ca667688d2df321f714f25d57788d
suvorovm <royming06@gmail.com>
2019-09-02 22:29:00 +0300

    rever

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn --bind='0.0.0.0:8080' hello:serverApp
+cd ask
+sudo gunicorn3 --bind='0.0.0.0:8080' ask.wsgi
+cd ..
+ 
+
