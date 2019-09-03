905318c791f4a88989fd887e5a63897f3cbb6510
suvorovm <royming06@gmail.com>
2019-09-03 21:16:46 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd ask
-sudo gunicorn3 --bind='0.0.0.0:8080' ask.wsgi
+sudo gunicorn --bind='0.0.0.0:8080' ask.wsgi
 cd ..
  
 
